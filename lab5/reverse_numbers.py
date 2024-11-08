2ab52855b7d138ceb27653934ddc5b64b64811d0
KENAYI <chikenayiweli@gmail.com>
2024-11-08 14:17:56 +0100

    more solutions for work

--- lab5/reverse_numbers.py
+++ lab5/reverse_numbers.py
@@ -1,3 +1,7 @@
+# Continuously take user input, split into words, and reverse the order of words
+# until '0' is entered as the first word in the line
+
+
 while True:
     # Read the input line and split it into a list of integers
     line = input().strip()
@@ -12,3 +16,39 @@ while True:
     
     # Print the reversed list with each number separated by a space
     print(" ".join(map(str, reversed_numbers)))
+
+# OR
+
+# # Read the first line of input and split it into a list of words
+# line = input("Enter a line of text (start with '0' to stop): ").split()
+
+# # Loop until the first word in 'line' is '0'
+# while line[0] != '0':
+#     # Reverse the list of words, excluding the first word
+#     rev = line[:0:-1]
+
+#     # Join the reversed list into a single string with spaces
+#     revstr = ' '.join(rev)
+
+#     # Print the reversed string
+#     print(revstr)
+
+#     # Read the next line of input and split it into a list of words
+#     line = input("Enter a line of text (start with '0' to stop): ").split()
+
+
+# OR
+
+# # Read the first line of input and split it into a list of words
+# line = input("Enter a line of numbers (start with '0' to stop): ").split()
+
+# # Loop until the first element in 'line' is '0'
+# while line[0] != '0':
+#     # Reverse the list of elements, excluding the first one
+#     reversenum = list(reversed(line[1:]))
+
+#     # Print the reversed list, unpacked into separate elements
+#     print(*reversenum)  # The * operator unpacks the list for space-separated output
+
+#     # Read the next line of input and split it into a list of words
+#     line = input("Enter a line of numbers (start with '0' to stop): ").split()
